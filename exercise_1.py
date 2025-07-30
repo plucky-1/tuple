6a069fd0e9541f70024dda12b5025e58c382951c
plucky-1 <bwehfaaaron@gmail.com>
2025-07-30 05:55:57 +0100

    tuple

---
+++ exercise_1.py
@@ -0,0 +1,23 @@
+#response =()
+#print(type(response))
+response = (True,"welcome",["email","username"])
+'''print(response[0])
+print(response[2])
+print(response[2][-1])
+print(type (response))
+response = list(response)
+print(type(response))
+response[0] = False
+print(type(response) , response)
+response = type(response)
+print(type(response) , response)
+
+print(response[1] + response[2][1])
+#a=[1,2,3]
+#b =[1,"2",3]
+#print(b[0] + b[1])
+#print(b[1] + b[1])
+a=[1,2,3,4]
+'''
+succes,message,data =response
+print(succes)
