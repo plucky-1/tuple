aeabf84bd3a0ea136d22e4d87f5ffb3114d83ee5
plucky-1 <bwehfaaaron@gmail.com>
2025-07-30 06:14:26 +0100

    tuple update

--- exercise_1.py
+++ exercise_1.py
@@ -1,7 +1,5 @@
-#response =()
-#print(type(response))
 response = (True,"welcome",["email","username"])
-'''print(response[0])
+print(response[0])
 print(response[2])
 print(response[2][-1])
 print(type (response))
@@ -9,15 +7,8 @@ response = list(response)
 print(type(response))
 response[0] = False
 print(type(response) , response)
-response = type(response)
+response = tuple(response)
 print(type(response) , response)
-
 print(response[1] + response[2][1])
-#a=[1,2,3]
-#b =[1,"2",3]
-#print(b[0] + b[1])
-#print(b[1] + b[1])
-a=[1,2,3,4]
-'''
 succes,message,data =response
 print(succes)
